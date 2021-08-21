aaceff9843490f78c6edd5773a5f19b7b415214f
Stephan Spiegel <stephan@stephanspiegel.com>
2021-08-21 18:35:24 -0400

    Move development tools to own plugin

--- lua/kraftwerk/init.lua
+++ lua/kraftwerk/init.lua
@@ -1,19 +1,11 @@
 local cmd = vim.cmd
 
 local function define_commands()
-    cmd("command! CheckVersion lua require'kraftwerk.system'.check_sfdx_version()")
     cmd("command! -range=% -nargs=? ForceDataSoqlQuery lua require'kraftwerk.data'.query(<range>, <line1>, <line2>, <q-args>)")
-    cmd("command! -range=% Test lua print(require'kraftwerk.util'.get_visual_selection())")
 end
 
 local function setup()
     define_commands()
-end
-
-function _G.dump(...)
-    local objects = vim.tbl_map(vim.inspect, {...})
-    print(unpack(objects))
-    return ...
 end
 
 return {
--- plugin/kraftwerk.vim
+++ plugin/kraftwerk.vim
@@ -5,15 +5,6 @@ endif
 let s:save_cpoptions = &cpoptions " save user coptions
 set cpoptions&vim " reset them to defaults
 
-" Convenience command for development of this plugin
-command! ReloadKraftwerk call Reload()
-
-function! Reload() abort
-	lua for k in pairs(package.loaded) do if k:match("^kraftwerk") then package.loaded[k] = nil end end
-	lua require("kraftwerk")
-    lua require("kraftwerk.init").setup()
-endfunction
-
 lua require("kraftwerk.init").setup()
 
 let &cpoptions = s:save_cpoptions " restore user coptions
