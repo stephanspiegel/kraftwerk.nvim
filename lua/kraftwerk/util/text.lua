b09139f70339200ec7149977b88974f49ebf02a4
Stephan Spiegel <stephan@stephanspiegel.com>
2021-08-30 05:46:50 -0400

    Update documentation comments

--- lua/kraftwerk/util/text.lua
+++ lua/kraftwerk/util/text.lua
@@ -8,7 +8,7 @@ Does the text start with a given prefix?
 @tparam prefix string The prefix to look for
 @Tparam text string The text we want to check
 @treturn boolean True if the text starts with the prefix, false otherwise
---]]
+]]
 local function starts_with(prefix, text)
     local function curried_function(str)
         return str:find(prefix, 1, true) == 1
@@ -19,10 +19,20 @@ local function starts_with(prefix, text)
     return curried_function(text)
 end
 
+--[[
+Remove any whitespace from the beginning and end of the input.
+@tparam text string The string from which to remove whitespace
+@treturn string The passed in input with leading and trailing whitespace removed
+]]
 local function trim(text)
     return string.gsub(text, '^%s*(.-)%s*$', '%1')
 end
 
+--[[
+Is the input either nil, empty or composed of only whitespace?
+@tparam text string The string we want to check
+@treturn boolean True if the string is nil, empty string or whitespace only, false otherwise
+]]
 local function is_blank(text)
     return text == nil or trim(text) == ''
 end
